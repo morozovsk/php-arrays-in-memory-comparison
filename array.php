92e788ec857895d74774beea4f67499e3a744cf1
Vladimir Goncharov <morozovsk@gmail.com>
2018-01-09 20:17:59 +0200

    Create array.php

---
+++ array.php
@@ -0,0 +1,22 @@
+<?php
+
+$start = time();
+
+$visits = [];
+
+$i = 1;
+while ($visitsData = @file_get_contents("data/visits_$i.json")) {
+    $visitsData = json_decode($visitsData, true);
+    foreach ($visitsData['visits'] as $k => $row) {
+        $visits[$row['id']] = $row;
+    }
+    $i++;echo "$i\n";
+}
+unset($visitsData);
+
+gc_collect_cycles();
+
+echo 'init time: ' . (time() - $start) . ', memory: ' . intval(memory_get_usage() / 1000000) . "\n";
+sleep(3600);
+
+//memory without indexes: 6057, init time: 39
